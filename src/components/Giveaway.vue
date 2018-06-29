b51d0c3ca8fbb08621735d8c3574f8a1a016f7dd
Sam <smills.smx@gmail.com>
2018-06-29 14:08:21 +0200

    Added Giveaway prototype.

--- src/components/Giveaway.vue
+++ src/components/Giveaway.vue
@@ -2,7 +2,7 @@
   <div class="giveaways">
     <h1>Giveaways</h1>
       <div class="giveaway">
-        <a class="e-widget no-button" :href="ga" rel="nofollow">Your Giveaway</a>
+        <a class="e-widget no-button" :href="this.$giveAway" rel="nofollow">Your Giveaway</a>
       </div>
   </div>
 </template>
@@ -12,16 +12,7 @@ export default {
   name: 'Giveaway',
   data () {
     return {
-      ga: ''
     }
-  },
-  methods: {
-    showGiveaway(){
-        this.ga = `https://gleam.io/6FH24/your-giveaway`
-    }
-  },
-  created: function(){
-    this.showGiveaway()
   }
 }
 </script>
--- src/main.js
+++ src/main.js
@@ -9,6 +9,7 @@ Vue.use(VueResource);
 Vue.prototype.$clientId = 'sy8t405is27qepl3jf8j7by99b3wo5k'
 Vue.prototype.$streamerId = '1'
 Vue.prototype.$twitch = 'quin69'
+Vue.prototype.$giveAway = 'https://gleam.io/6FH24/your-giveaway'
 
 /* eslint-disable no-new */
 new Vue({
